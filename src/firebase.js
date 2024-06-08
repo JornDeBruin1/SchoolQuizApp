22e3c5da1213b854ae73c2eba6ed2ab889af1b50
Jorn de Bruin <jorn.bruin@student.graafschapcollege.nl>
2024-06-08 19:48:44 +0200

    firebase hosting added

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+import { initializeApp } from 'firebase/app';
+const config = {
+	apiKey: 'AIzaSyBLNV_blD7QtwRYaTkZH96e1eKcMWU1BK8',
+	authDomain: 'schoolquiz1.firebaseapp.com',
+	projectId: 'schoolquiz1',
+	storageBucket: 'schoolquiz1.appspot.com',
+	messagingSenderId: '192216687056',
+	appId: '1:192216687056:web:feee60c6865eadb1e4d9d5',
+};
+const firebaseApp = initializeApp(config);
